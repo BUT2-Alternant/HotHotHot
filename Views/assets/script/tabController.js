85794eddb82e7714cd97323e37ccce85ed1ce335
Thibaut LAHAIE <thibaut.lahaie@etu.univ-amu.fr>
2023-06-18 17:25:18 +0200

    fix(tabController): display data when they are loaded

--- Views/assets/script/tabController.js
+++ Views/assets/script/tabController.js
@@ -1,17 +1,17 @@
+let donneeChargee = false;
+
 /**
  * Fonction qui permet de changer le contenu affiché a l'écran en fonction de l'onglet cliqué
  * @param event
  */
 function changerOnglet(event) {
-    let donneeChargee = localStorage.getItem("donneeChargee");
-
     let ongletDonnee = document.getElementById('donnee');
     let boutonDonnee = document.getElementById('bouton-donnee');
 
     let ongletHistorique = document.getElementById('historique');
     let boutonHistorique = document.getElementById('bouton-historique');
 
-    if (!donneeChargee) {
+    if (donneeChargee) {
         if (event.target.innerText === "Données") {
             ongletDonnee.style.display = "block"
             ongletDonnee.ariaDisabled = "false"
@@ -36,4 +36,32 @@ function changerOnglet(event) {
     } else {
         alert("Les données ne sont pas encore chargées, veuillez patienter");
     }
+}
+
+/**
+ * Fonction permetant de rendre visible la partie donnée et de cacher la partie historique et chargement des données
+ */
+function afficherDonnee() {
+    donneeChargee = true;
+
+    let chargement = document.getElementById('chargement');
+
+    let ongletDonnee = document.getElementById('donnee');
+    let boutonDonnee = document.getElementById('bouton-donnee');
+
+    let ongletHistorique = document.getElementById('historique');
+    let boutonHistorique = document.getElementById('bouton-historique');
+
+    ongletDonnee.style.display = "block";
+    ongletDonnee.ariaDisabled = "false";
+    boutonDonnee.style.backgroundColor = "#97B2EC";
+
+    ongletHistorique.style.display = "none";
+    ongletHistorique.ariaDisabled = "true";
+    boutonHistorique.style.backgroundColor = "#D3E0EF";
+
+    chargement.style.display = "none";
+    chargement.ariaDisabled = "true";
+
+    localStorage.setItem("ongletCourant", "data");
 }
